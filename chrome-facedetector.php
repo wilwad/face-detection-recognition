712d0061cba07841bb481289f252b9b760675ad5
William Sengdara <william.sengdara@gmail.com>
2018-07-14 14:51:45 +0200

    Create chrome-facedetector.php

---
+++ chrome-facedetector.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+ <title>Chrome FaceDetector</title>
+</head>
+<body>
+ <div>
+  <img id='img'>
+ </div>
+ <HR>
+ <div id='images'>
+ <?php
+ ?>
+ <div>
+ <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+ <script>
+  $(document).ready(function(){
+    
+  });
+ </script>
+</body>
+<html>
